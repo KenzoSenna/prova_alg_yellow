264fff2b0ab9a321bea172179d9d0d94742d4939
Kenzo de Oliveira Senna <kenzoandblade@gmail.com>
2025-07-15 22:43:58 -0400

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -12,8 +12,8 @@ def verificadora_leitura(lista, quantidade_min_pag = 30):
         itens_do_dict = [] 
         for itens in dicionario.values():
             itens_do_dict.append(itens)
-        if itens[1] > quantidade_min_pag:
-            lista_nomes_grandes_leitores.append(itens[0])
+        if itens_do_dict[1] > quantidade_min_pag:
+            lista_nomes_grandes_leitores.append(itens_do_dict[0])
     return lista_nomes_grandes_leitores
 
 print(f"O nome do(s) usuário(s) que superam o limite mínimo de páginas lidas pré-estabelecidos é de: {verificadora_leitura(lista_dict)}")
